7791e1aa8de4f2cc84045dd6c05b5c64d5c7e874
diasfulvio <dias.fulvio@gmail.com>
2015-09-30 12:56:00 -0300

    site

---
+++ WebAppSitePackages/Controllers/HomeController.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Canducci.Zip;
+namespace WebAppSitePackages.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ZipCodeInfo zipcodeInfo = ZipCodeLoad.Find("19200000");
+            ViewBag.ZipCodeInfo = zipcodeInfo;
+            return View();
+        }
+    }
+}
