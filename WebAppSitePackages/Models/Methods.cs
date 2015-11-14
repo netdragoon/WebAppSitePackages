e9f13ccdb5ea5e898a7920f4ea0d88fa5b3201d2
netdragoon <netdragoon@outlook.com>
2015-11-14 11:30:33 -0200

    new

---
+++ WebAppSitePackages/Models/Methods.cs
@@ -0,0 +1,15 @@
+﻿namespace System
+{
+    public static class Methods
+    {
+        public static string WithoutAccents(this string str)
+        {
+            if (string.IsNullOrEmpty(str))
+            {
+                return string.Empty;
+            }
+            byte[] bytes = Text.Encoding.GetEncoding("iso-8859-8").GetBytes(str);
+            return Text.Encoding.UTF8.GetString(bytes);
+        }
+    }
+}
